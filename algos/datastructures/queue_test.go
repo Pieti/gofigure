feb34f9e04d06ee86d920d39cd03597183dd0a70
Niko Pietikäinen <niko.pietikainen@protonmail.com>
2023-07-10 18:52:12 +0200

    Queue and Stack

---
+++ algos/datastructures/queue_test.go
@@ -0,0 +1,26 @@
+package datastructures
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestQueue(t *testing.T) {
+	q := NewQueue()
+
+	q.Enqueue(5)
+	q.Enqueue(7)
+	q.Enqueue(9)
+
+	assert.Equal(t, 5, q.Dequeue())
+	assert.Equal(t, q.size, 2)
+
+	q.Enqueue(11)
+	assert.Equal(t, q.Dequeue(), 7)
+	assert.Equal(t, q.Dequeue(), 9)
+	assert.Equal(t, q.Peek(), 11)
+	assert.Equal(t, q.Dequeue(), 11)
+	assert.Equal(t, q.Dequeue(), nil)
+	assert.Equal(t, q.size, 0)
+
+}
---
+++ algos/datastructures/stack_test.go
@@ -0,0 +1,26 @@
+package datastructures
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestStack(t *testing.T) {
+	s := NewStack()
+
+	s.Push(5)
+	s.Push(7)
+	s.Push(9)
+
+	assert.Equal(t, s.Pop(), 9)
+	assert.Equal(t, s.size, 2)
+
+	s.Push(11)
+	assert.Equal(t, s.Pop(), 11)
+	assert.Equal(t, s.Pop(), 7)
+	assert.Equal(t, s.Peek(), 5)
+	assert.Equal(t, s.Pop(), 5)
+	assert.Equal(t, s.Pop(), nil)
+	assert.Equal(t, s.size, 0)
+
+}
