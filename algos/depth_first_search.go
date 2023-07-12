ca217df12c29418fa1baf0a255f2b976a1009fd5
Niko Pietikäinen <nikpie@proton.me>
2023-07-12 19:04:42 +0200

    Depth first

---
+++ algos/depth_first_search.go
@@ -0,0 +1,17 @@
+package main
+
+func depthFirstSearch(head *BinaryNode, needle int) bool {
+	if head == nil {
+		return false
+	}
+
+	if head.value == needle {
+		return true
+	}
+
+	return depthFirstSearch(head.left, needle) || depthFirstSearch(head.right, needle)
+}
+
+func DepthFirstSearch(head *BinaryNode, needle int) bool {
+	return depthFirstSearch(head, needle)
+}
